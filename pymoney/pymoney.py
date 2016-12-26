1735b9d2b70c860c3af2f14896b0b1805f3f6dd3
Holger Rother <hrother@hrother.org>
2016-12-26 08:40:18 +0100

    Initialization of Money object.

--- pymoney/pymoney.py
+++ pymoney/pymoney.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*-
+import decimal
+from decimal import Decimal as D
+
+from .exceptions import InvalidAmount
+
+
+class Money(object):
+    """Representation of a monetary value. Money consists of an decimal amount
+    and an currency.
+
+    The :attr:`amount` of :class:`Money` is rounded using
+    :attr:`rounding_method` with :attr:`cent_factor`. Which default to
+    :attr:`rounding_method` = decimal.ROUND_HALF_EVEN and 
+    :attr:`cent_factor` = '.01'
+
+    :param amount: of money
+    :type amount: numeric
+    :param str currency: In which the given `amount` is denoted.
+
+    """
+
+    cent_factor = '.01'
+    rounding_method = decimal.ROUND_HALF_EVEN
+
+    def __init__(self, amount, currency):
+        try:
+            amount = D(amount)
+        except decimal.InvalidOperation:
+            raise InvalidAmount(
+                'Not possible to create {} with amount {}'.format(
+                    self.__class__.__name__, amount))
+
+        self.amount = D(amount.quantize(D(self.cent_factor),
+                                        rounding=self.rounding_method))
+        self.currency = currency
+
+    def __repr__(self):
+        return '{}(amount={!r}, currency={!r})'.format(
+                self.__class__.__name__,
+                self.amount, self.currency)
--- tests/test_pymoney.py
+++ tests/test_pymoney.py
@@ -8,21 +8,57 @@ test_pymoney
 Tests for `pymoney` module.
 """
 
-
-import sys
-import unittest
+import pytest
+from decimal import Decimal as D
 
 from pymoney import pymoney
+from pymoney.exceptions import InvalidAmount
 
 
+def test_money_init_decimal_amount():
+    m = pymoney.Money(D('10'), 'EUR')
+    assert m.amount == D('10')
+    assert m.currency == 'EUR'
 
-class TestPymoney(unittest.TestCase):
 
-    def setUp(self):
-        pass
+def test_money_init_string_amount():
+    m = pymoney.Money('10', 'EUR')
+    assert m.amount == D('10')
+    assert m.currency == 'EUR'
 
-    def tearDown(self):
-        pass
 
-    def test_000_something(self):
-        pass
+def test_money_init_integer_amount():
+    m = pymoney.Money(10, 'EUR')
+    assert m.amount == D('10')
+    assert m.currency == 'EUR'
+
+
+def test_money_init_float_amount():
+    m = pymoney.Money(10.0, 'EUR')
+    assert m.amount == D('10')
+    assert m.currency == 'EUR'
+
+
+def test_money_init_invalid_amount():
+    with pytest.raises(InvalidAmount):
+        _ = pymoney.Money('9,231', 'EUR')
+
+
+def test_money_init_with_changed_cent_factor():
+    old_precision = pymoney.Money.cent_factor
+    pymoney.Money.cent_factor = '.001'
+    m = pymoney.Money(D('10.00123231'), 'EUR')
+    assert m.amount == D('10.001')
+    assert m.currency == 'EUR'
+    pymoney.Money.cent_factor = old_precision
+
+
+def test_money_init_rounds_amount_to_cent_factor():
+    m = pymoney.Money(D('10.00123231'), 'EUR')
+    assert m.amount == D('10.00')
+    assert m.currency == 'EUR'
+
+
+def test_money_repr():
+    m = pymoney.Money(D('42'), 'EUR')
+    assert "Money(amount=Decimal('42.00'), currency='EUR')" == repr(m)
